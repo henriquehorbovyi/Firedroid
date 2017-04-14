f3c258a479ed5ee9bace37bc797bd167c3b07455
henrywm <carloshenry.wm@gmail.com>
2017-04-14 02:23:30 -0300

    Created a model

--- app/src/main/java/com/app/henry/firedroid/model/User.java
+++ app/src/main/java/com/app/henry/firedroid/model/User.java
@@ -5,4 +5,49 @@ package com.app.henry.firedroid.model;
  */
 
 public class User {
+
+    private int id;
+    private String name;
+    private String email;
+    private String pass;
+
+    public User(){}
+    public User(int id, String name, String email, String pass) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.pass = pass;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
 }
